695d51bb74c8c9a5827405426a08f913f1810207
Katka1111 <ksnopkova@novis.eu>
2024-10-15 09:52:29 +0200

    10

--- components/charts-01.tsx
+++ components/charts-01.tsx
@@ -692,7 +692,7 @@ export function Charts() {
           <CardHeader className="p-4 pb-0">
             <CardTitle>Active Energy</CardTitle>
             <CardDescription>
-              You're burning an average of 754 calories per day. Good job!
+              You are burning an average of 754 calories per day. Good job!
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-2">
